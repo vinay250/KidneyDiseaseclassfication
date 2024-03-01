c6caf05d5c7c3d17db31e25b30f8e8c8b32bd62f
“vinayaka <vinayakavirat008@gmail.com>
2024-03-01 17:17:26 +0530

    updated

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -20,7 +20,7 @@ class DataIngestion:
         try: 
             dataset_url = self.config.source_URL
             zip_download_dir = self.config.local_data_file
-            os.makedirs("KidneyDiseaseclassfication/research/artifacts/data_ingestion", exist_ok=True)
+            os.makedirs("artifacts/data_ingestion", exist_ok=True)
             logger.info(f"Downloading data from {dataset_url} into file {zip_download_dir}")
 
             file_id = dataset_url.split("/")[-2]

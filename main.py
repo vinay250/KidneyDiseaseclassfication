a6cf5c06356ecbebe39074e1039cc5e02e1fafef
“vinayaka <vinayakavirat008@gmail.com>
2024-03-04 19:11:13 +0530

    models updated

--- main.py
+++ main.py
@@ -43,15 +43,3 @@ except Exception as e:
         raise e
 
 
-
-STAGE_NAME = "Evaluation stage"
-try:
-   logger.info(f"*******************")
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   model_evalution = EvaluationPipeline()
-   model_evalution.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-
-except Exception as e:
-        logger.exception(e)
-        raise e

1e58e1089166db5e1f3cbba48fe2bef27c246150
“vinayaka <vinayakavirat008@gmail.com>
2024-03-05 10:50:34 +0530

    modles updated

--- app.py
+++ app.py
@@ -1,44 +1,53 @@
-from flask import Flask, request, jsonify, render_template, send_from_directory
+from flask import Flask, request, jsonify, render_template
+import os
 from flask_cors import CORS, cross_origin
 from cnnClassifier.utils.common import decodeImage
 from cnnClassifier.pipeline.prediction import PredictionPipeline
-import os
+
+
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
 
 app = Flask(__name__)
 CORS(app)
+
 
 class ClientApp:
     def __init__(self):
         self.filename = "inputImage.jpg"
         self.classifier = PredictionPipeline(self.filename)
 
+
 @app.route("/", methods=['GET'])
 @cross_origin()
 def home():
     return render_template('index.html')
 
-@app.route("/train", methods=['GET', 'POST'])
+
+
+
+@app.route("/train", methods=['GET','POST'])
 @cross_origin()
 def trainRoute():
-    # Add code here to trigger training
+    os.system("python main.py")
+    # os.system("dvc repro")
     return "Training done successfully!"
+
+
 
 @app.route("/predict", methods=['POST'])
 @cross_origin()
 def predictRoute():
-    image = request.json.get('image')
-    if not image:
-        return jsonify({'error': 'No image provided'}), 400
-
+    image = request.json['image']
     decodeImage(image, clApp.filename)
     result = clApp.classifier.predict()
     return jsonify(result)
 
-# Serve static files like images or CSS
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    return send_from_directory('static', filename)
 
 if __name__ == "__main__":
     clApp = ClientApp()
-    app.run(host='0.0.0.0', port=8080)  # for AWS
+
+    app.run(host='0.0.0.0', port=8080) #for AWS
+
+
